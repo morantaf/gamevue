298e59f89b979037d73a3f261324bff47e354749
morantaf <fall_moranta@live.fr>
2020-08-20 16:26:04 +0200

    Changing isOpen according to mouseleave or mouseover

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,82 @@
+<template>
+  <div
+    class="menu-item"
+    @mouseover="isOpen = true"
+    @mouseleave="isOpen = false"
+  >
+    <a href="#">
+      {{ title }}
+      <i class="fas fa-caret-down"></i>
+    </a>
+    <transition name="fade" appear>
+      <div
+        class="sub-menu"
+        v-if="isOpen"
+        @mouseover="isOpen = true"
+        @mouseleave="isOpen = false"
+      >
+        <div v-for="(item, i) in items" :key="i" class="dropdown-item">
+          <router-link :to="`/categories/${item.slug}`">{{
+            item.title
+          }}</router-link>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Dropdown extends Vue {
+  @Prop() title!: string;
+  @Prop() items!: string;
+  isOpen = false;
+}
+</script>
+
+<style>
+nav .menu-item svg {
+  width: 10px;
+  margin-left: 10px;
+}
+
+.menu-item a {
+  text-decoration: none;
+  padding: 20px;
+  color: inherit;
+  font-weight: bold;
+  transition-duration: 0.5s;
+  width: 100%;
+}
+.menu-item a:hover {
+  background-color: #ccc;
+}
+
+.dropdown-item {
+  padding: 20px 0;
+  border-bottom: 1px solid #dae4eb;
+}
+
+.dropdown-item:hover {
+  background-color: #ccc;
+}
+
+nav .sub-menu {
+  position: absolute;
+  background-color: #fff;
+  left: 50%;
+  top: calc(80%);
+  transform: translateX(-59%);
+  width: max-content;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease-out;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
