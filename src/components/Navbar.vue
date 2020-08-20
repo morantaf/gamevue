de329a6d4f1e221a11c84318a9cffc9b229f9c50
morantaf <fall_moranta@live.fr>
2020-08-20 12:40:32 +0200

    Adding the Dropdown component to the Navbard and adding the categories' list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,77 @@
 <template>
-  <div id="nav">
+  <nav id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/categories">Categories</router-link> |
+    <Dropdown v-bind:title="'Categories'" v-bind:items="listOfCategories" />|
     <router-link to="/random">Random</router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import Dropdown from "./Dropdown.vue";
+import request from "superagent";
 
-@Component
-export default class Navbae extends Vue {
-  msg = "NavBar";
+@Component({
+  components: {
+    Dropdown,
+  },
+})
+export default class Navbar extends Vue {
+  listOfCategories = [
+    {
+      title: "Adventure",
+      slug: "adventure",
+    },
+    {
+      title: "RPG",
+      slug: "rpg",
+    },
+    {
+      title: "Action",
+      slug: "action",
+    },
+    {
+      title: "Shooter",
+      slug: "shooter",
+    },
+  ];
+
+  // async fetchCategories() {
+  //   try {
+  //     const response = await request
+  //       .get(`https://rawg-video-games-database.p.rapidapi.com/genres`)
+  //       .set("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com")
+  //       .set(
+  //         "x-rapidapi-key",
+  //         "58fc62e76dmsh80ce19cd8cbea27p16bd17jsnbbc7c11d51dc"
+  //       );
+
+  //     this.listOfCategories = response.body.results.map((category: any) => ({
+  //       title: category.name,
+  //       slug: category.slug,
+  //     }));
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+
+  // mounted() {
+  //   this.fetchCategories();
+  // }
 }
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+#nav {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: #fff;
+  border-bottom: 1px solid #dae4eb;
+  display: flex;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
 }
