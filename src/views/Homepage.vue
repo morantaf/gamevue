f00cf98470f76e5e141cd28026b33d486203a362
morantaf <fall_moranta@live.fr>
2020-08-21 11:55:25 +0200

    Adding the banner to the page

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
+    <div class="banner">
+      <h1>Welcome to gamezone</h1>
+      <h3>The best video game ressources of the internet</h3>
+    </div>
     <GamesList :home="home" />
   </div>
 </template>
@@ -20,3 +24,26 @@ export default {
   },
 };
 </script>
+
+<style>
+.banner {
+  background-image: url("https://wallpapercave.com/wp/wp6082158.jpg");
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  height: 700px;
+  margin-top: 90px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.banner h1 {
+  font-size: 5em;
+}
+
+.banner h3 {
+  font-size: 2em;
+}
+</style>
