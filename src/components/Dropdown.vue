7c129a454a21de0cc1aa2f20da49e83df9ff07dd
morantaf <fall_moranta@live.fr>
2020-08-21 12:03:29 +0200

    Changing the colors

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -15,7 +15,12 @@
         @mouseover="isOpen = true"
         @mouseleave="isOpen = false"
       >
-        <div v-for="(item, i) in items" :key="i" class="dropdown-item">
+        <div
+          v-for="(item, i) in items"
+          :key="i"
+          class="dropdown-item"
+          @click="isOpen = false"
+        >
           <router-link :to="`/categories/${item.slug}`">{{
             item.title
           }}</router-link>
@@ -51,21 +56,22 @@ nav .menu-item svg {
   width: 100%;
 }
 .menu-item a:hover {
-  background-color: #ccc;
+  background-color: #414958;
 }
 
 .dropdown-item {
+  background-color: #626d84;
   padding: 20px 0;
   border-bottom: 1px solid #dae4eb;
 }
 
 .dropdown-item:hover {
-  background-color: #ccc;
+  background-color: #414958;
 }
 
 nav .sub-menu {
   position: absolute;
-  background-color: #fff;
+  background-color: #414958;
   left: 50%;
   top: calc(80%);
   transform: translateX(-59%);
