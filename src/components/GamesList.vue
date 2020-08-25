c83f7f888148ade49736f4f279ec464c91fb2ed0
morantaf <fall_moranta@live.fr>
2020-08-25 14:35:51 +0200

    Adding sorting dropdown, refactoring the fetchGames function, styling the dropdown

--- src/components/GamesList.vue
+++ src/components/GamesList.vue
@@ -7,19 +7,41 @@
     <div v-if="error" class="error">
       {{ error }}
     </div>
-    <div class="games-list" v-if="home">
-      <GameCard
-        v-for="game in reducedGamesList"
-        :key="game.id"
-        v-bind:id="game.id"
-        v-bind:name="game.name"
-        v-bind:image="game.backgroundImage"
-        v-bind:platforms="game.platforms"
-        v-bind:rating="game.rating"
-      ></GameCard>
-    </div>
 
-    <div class="games-list" v-else>
+    <!-- <div class="games-list" v-if="home">
+    <GameCard
+      v-for="game in reducedGamesList"
+      :key="game.id"
+      v-bind:id="game.id"
+      v-bind:name="game.name"
+      v-bind:image="game.backgroundImage"
+      v-bind:platforms="game.platforms"
+      v-bind:rating="game.rating"
+    ></GameCard>
+  </div> -->
+    <h1 v-if="category">{{ categoryTitle }}</h1>
+
+    <div
+      class="sort-options"
+      @mouseover="sortingMenuOpen = true"
+      @mouseleave="sortingMenuOpen = false"
+    >
+      <div class="selected">Sort by <i class="fas fa-caret-down"></i></div>
+      <div
+        class="option-container"
+        v-if="sortingMenuOpen"
+        @mouseover="sortingMenuOpen = true"
+        @mouseleave="sortingMenuOpen = false"
+      >
+        <div class="option" @click="sortLowestRating">Rating ascending</div>
+        <div class="option" @click="sortHighestRating">Rating descending</div>
+        <div class="option" @click="sortByNameAscending">Name ascending</div>
+        <div class="option" @click="sortByNameDescending">
+          Name descending
+        </div>
+      </div>
+    </div>
+    <div class="games-list">
       <GameCard
         v-for="game in gamesList"
         :key="game.id"
@@ -38,17 +60,28 @@ import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import * as request from "superagent";
 import GameCard from "./GameCard.vue";
 
+const API_KEY = process.env.VUE_APP_API_KEY;
+
 type Game = {
   id: number;
   name: string;
   backgroundImage: string;
   platforms: Platform[];
   rating: number;
+  genres: Genre[];
 };
 
 type Platform = {
   id: number;
   name: string;
+  slug: string;
+};
+
+type Genre = {
+  games_count: number;
+  id: number;
+  name: string;
+  background_image: string;
   slug: string;
 };
 
@@ -58,27 +91,51 @@ type Platform = {
   },
 })
 export default class GamesList extends Vue {
-  gamesList = [];
+  gamesList: Game[] = [];
   loading = false;
   error = null;
+  sortingMenuOpen = false;
   @Prop() category!: string;
   @Prop() home!: boolean;
   @Prop() displayedGameGenres!: string[];
   @Prop() displayedGameName!: string;
 
-  get reducedGamesList(): Game[] {
-    const maxIndex = this.gamesList.length - 11;
-    const minIndex = 1;
-    const randomIndex = Math.floor(
-      Math.random() * (maxIndex - minIndex) + minIndex
-    );
-    return this.gamesList.splice(randomIndex, 10);
+  get categoryTitle(): string {
+    const category = this.category;
+
+    const capitalStr =
+      category === "rpg"
+        ? category.toUpperCase()
+        : category.charAt(0).toUpperCase() + category.slice(1);
+    return capitalStr;
   }
 
-  get recommandationList(): Game[] {
-    const maxIndex = this.gamesList.length - 4;
-    const randomIndex = Math.floor(Math.random() * (maxIndex - 1) + 1);
-    return this.gamesList.splice(randomIndex, 3);
+  // get reducedGamesList(): Game[] {
+  //   const copy = this.gamesList.slice(0);
+  //   const maxIndex = copy.length - 11;
+  //   const minIndex = 1;
+  //   const randomStartIndex = Math.floor(
+  //     Math.random() * (maxIndex - minIndex) + minIndex
+  //   );
+  //   const endIndex = randomStartIndex + 10;
+  //   return copy.slice(randomStartIndex, endIndex);
+  // }
+
+  sortLowestRating() {
+    this.gamesList.sort((a: Game, b: Game) => a.rating - b.rating);
+    this.sortingMenuOpen = false;
+  }
+  sortHighestRating() {
+    this.gamesList.sort((a: Game, b: Game) => b.rating - a.rating);
+    this.sortingMenuOpen = false;
+  }
+  sortByNameAscending() {
+    this.gamesList.sort((a: Game, b: Game) => (a.name > b.name ? -1 : 1));
+    this.sortingMenuOpen = false;
+  }
+  sortByNameDescending() {
+    this.gamesList.sort((a: Game, b: Game) => (a.name < b.name ? -1 : 1));
+    this.sortingMenuOpen = false;
   }
 
   async fetchGames(): Promise<Game[] | undefined> {
@@ -87,74 +144,54 @@ export default class GamesList extends Vue {
       const response = await request
         .get(`https://rawg-video-games-database.p.rapidapi.com/games`)
         .set("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com")
-        .set(
-          "x-rapidapi-key",
-          "58fc62e76dmsh80ce19cd8cbea27p16bd17jsnbbc7c11d51dc"
-        );
-      const dataReceived = response.body;
+        .set("x-rapidapi-key", `${API_KEY}`);
+      const receivedData = response.body;
 
       this.loading = false;
 
-      return (this.gamesList = dataReceived.results.map((game: any) => {
+      return receivedData.results.map((game: any) => {
         return {
           id: game.id,
           name: game.name,
           backgroundImage: game.background_image,
           platforms: game.parent_platforms,
           rating: game.rating,
+          genres: game.genres,
         };
-      }));
-    } catch (e) {
-      this.error = e.toString();
-      console.error(e);
-    }
-  }
-
-  async fetchGamesAndFilterByCategory(
-    category: string
-  ): Promise<Game[] | undefined> {
-    try {
-      this.loading = true;
-      const response = await request
-        .get(`https://rawg-video-games-database.p.rapidapi.com/games`)
-        .set("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com")
-        .set(
-          "x-rapidapi-key",
-          "58fc62e76dmsh80ce19cd8cbea27p16bd17jsnbbc7c11d51dc"
-        );
-      const dataReceived = response.body;
-
-      this.loading = false;
-      const gamesListReceived = dataReceived.results;
-      const filteredGamesList = gamesListReceived.filter((game: any) => {
-        const genresList = game.genres;
-        const formattedGenreList = genresList.map((genre: any) =>
-          genre.name.toLowerCase()
-        );
-
-        if (formattedGenreList.includes(this.category)) {
-          return true;
-        }
       });
-      return (this.gamesList = filteredGamesList.map((game: any) => {
-        return {
-          id: game.id,
-          name: game.name,
-          backgroundImage: game.background_image,
-          platforms: game.parent_platforms,
-          rating: game.rating,
-        };
-      }));
     } catch (e) {
       this.error = e.toString();
       console.error(e);
     }
   }
 
-  mounted() {
-    this.category
-      ? this.fetchGamesAndFilterByCategory(this.category)
-      : this.fetchGames();
+  reduceGamesList(list: Game[] | undefined): Game[] {
+    return list.slice(0, 10);
+  }
+
+  filterByCategory(list: Game[] | undefined): Game[] {
+    return list.filter((game: any) => {
+      const genresList = game.genres;
+      const formattedGenreList = genresList.map((genre: any) =>
+        genre.name.toLowerCase()
+      );
+
+      if (formattedGenreList.includes(this.category)) {
+        return true;
+      }
+    });
+  }
+
+  async mounted() {
+    try {
+      const receivedGamesList = await this.fetchGames();
+
+      this.gamesList = this.category
+        ? this.filterByCategory(receivedGamesList)
+        : this.reduceGamesList(receivedGamesList);
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 </script>
@@ -164,5 +201,43 @@ export default class GamesList extends Vue {
   display: flex;
   flex-wrap: wrap;
   margin-top: 90px;
+}
+
+.sort-options {
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  width: 150px;
+  margin-left: 50px;
+  position: relative;
+  top: 20px;
+}
+
+.selected {
+  background-color: #fff;
+  width: 100%;
+  color: #000;
+}
+
+.option-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: #fff;
+  color: #000;
+  cursor: pointer;
+  position: absolute;
+  top: 100%;
+}
+
+.option {
+  padding: 5px 0;
+  align-self: start;
+  width: 100%;
+}
+
+.option:hover {
+  background-color: #80bfff;
+  color: #fff;
 }
 </style>
